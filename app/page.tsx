5ec4d0a83f6d253b9ebea8e38b9336224d6e667a
katya havriuk <0303gavkat@gmail.com>
2024-11-20 14:13:08 -0700

    navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Link from '@/node_modules/next/link';
+import Navigation from './components/Navigation';
+
 export default function Home() {
   return (
-   <h1>hello next</h1>
+    <>
+      <h1>hello next</h1>
+      <Navigation />
+    </>
+
   );
 }
