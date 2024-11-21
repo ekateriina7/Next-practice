37f2fa17657e061664dffe9ad9c5d38f2f73cdbc
katya havriuk <0303gavkat@gmail.com>
2024-11-21 12:20:28 -0700

    data fetching

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import UsersCount from '../components/UsersCount';
 
-export default function page() {
+export default async function page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json()
+  console.log(data)
   return (
-    <div>cabins</div>
+    <div>cabins
+      <UsersCount users={data}/>
+    </div>
   )
 }
