f47876bddefdbb1f33f685f25ea2d4262209334c
katya havriuk <0303gavkat@gmail.com>
2024-11-26 16:39:34 -0700

    error boundaries

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { FC } from 'react';
+
+interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
+
+const Error: FC<ErrorProps> = ({ error, reset }) => {
+  return (
+    <main className="flex justify-center items-center flex-col gap-6">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+      <p className="text-lg">{error.message}</p>
+
+      <button
+        onClick={reset}
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+      >
+        Try again
+      </button>
+    </main>
+  );
+};
+
+export default Error;
