2a18b36c9ca30291a9b32235861f42e6b46c84dd
katya havriuk <0303gavkat@gmail.com>
2024-11-27 14:45:25 -0700

    async component fix

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -1,16 +1,16 @@
-import { getCabins } from '../_lib/data-service';
-import CabinCard from './CabinCard';
+import React from 'react';
 import { Cabin } from '../_types/cabin';
+import CabinCard from './CabinCard';
 
-export default async function CabinList() {
-  const cabins: Cabin[] = await getCabins();
-
-  if (!cabins.length) return <p>No cabins available.</p>;
+export default function CabinList({ cabins }: { cabins: Cabin[] }) {
+  if (!cabins.length) {
+    return <p>No cabins available.</p>;
+  }
 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
       {cabins.map((cabin) => (
-        <CabinCard cabin={cabin} key={cabin.id} />
+        <CabinCard key={cabin.id} cabin={cabin} />
       ))}
     </div>
   );
--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,12 +1,15 @@
 import React, { Suspense } from 'react';
+import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
-//import CabinList from '../_components/CabinList';
+import { getCabins } from '../_lib/data-service';
 
 export const metadata = {
   title: 'Cabins',
 };
 
-export default function Page() {
+export default async function Page() {
+  const cabins = await getCabins();
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
@@ -14,7 +17,7 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian Dolomites. Imagine waking up to beautiful mountain views, spending your days exploring the dark forests around, or just relaxing in your private hot tub under the stars. Enjoy nature&apos;s beauty in your own little home away from home. The perfect spot for a peaceful, calm vacation. Welcome to paradise.
       </p>
       <Suspense fallback={<Spinner />}>
-        {/* <CabinList /> */}
+        <CabinList cabins={cabins} />
       </Suspense>
     </div>
   );
