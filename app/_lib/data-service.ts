1bfa637a0e9ff695bcbfffa7249b9224267aebf0
Kate Havriuk <katehavriuk@Kates-MacBook-Pro.local>
2024-12-12 14:44:08 -0700

    server component inside client component

--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -135,17 +135,17 @@ export const getCabins = async function (): Promise<Cabin[]> {
 //   return data;
 // }
 
-// export async function getCountries() {
-//   try {
-//     const res = await fetch(
-//       'https://restcountries.com/v2/all?fields=name,flag'
-//     );
-//     const countries = await res.json();
-//     return countries;
-//   } catch {
-//     throw new Error('Could not fetch countries');
-//   }
-// }
+export async function getCountries() {
+  try {
+    const res = await fetch(
+      'https://restcountries.com/v2/all?fields=name,flag'
+    );
+    const countries = await res.json();
+    return countries;
+  } catch {
+    throw new Error('Could not fetch countries');
+  }
+}
 
 // /////////////
 // // CREATE
