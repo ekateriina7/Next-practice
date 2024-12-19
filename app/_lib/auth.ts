ebe82f17b367a6530084736cdef10112465e981e
katya havriuk <0303gavkat@gmail.com>
2024-12-19 16:56:33 -0700

    protected route

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,5 +1,11 @@
+import { NextApiRequest } from 'next';
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { User } from '../_types/user';
+
+interface Auth {
+  user?: User;
+}
 
 const authConfig = {
   providers: [
@@ -7,7 +13,12 @@ const authConfig = {
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    authorized({ auth, request }: { auth: Auth; request: NextApiRequest }) {
+      return !!auth?.user
+    }
+  }
 }
 
 export const { auth, handlers: { GET, POST } } = NextAuth(authConfig)
