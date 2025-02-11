5c317cb4363f651db882b370769d958d6869e027
katya havriuk <0303gavkat@gmail.com>
2025-02-11 14:53:40 -0700

    deleting spinner

--- app/_components/DeleteReservation.tsx
+++ app/_components/DeleteReservation.tsx
@@ -2,16 +2,33 @@
 
 import { TrashIcon } from '@heroicons/react/24/solid';
 import { deleteReservation } from '../_lib/actions';
+import { useTransition } from 'react';
+import Spinner from './Spinner';
+import MiniSpinner from './MiniSpinner';
 
 interface DeleteReservationProps {
   bookingId: string;
 }
 
 function DeleteReservation({ bookingId }: DeleteReservationProps) {
+  const [isPending, startTransition] = useTransition();
+  const handleDelete = () => {
+    if (confirm('Are you sure you want to delete this reservation?')) {
+      startTransition(() => {
+        deleteReservation(bookingId);
+      });
+    }
+  };
   return (
-    <button onClick={() => deleteReservation(bookingId)} className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
-      <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-      <span className='mt-1'>Delete</span>
+    <button onClick={handleDelete} className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
+      {!isPending ? (
+        <>
+          <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
+          <span className='mt-1'>Delete</span>
+        </>
+      ) : (
+        <span className='mx-auto py-1.5'><MiniSpinner /></span>
+      )}
     </button>
   );
 }
---
+++ app/_components/MiniSpinner.tsx
@@ -0,0 +1,10 @@
+function MiniSpinner() {
+  return (
+    <div
+      className="h-4 w-4 animate-spin rounded-full 
+        border-2 border-primary-800 border-r-transparent"
+    />
+  );
+}
+
+export default MiniSpinner; 
